c907859ef4e9e455cd5de16ed38970f5df366da0
Matheus Melquiades <matheusmelqn@gmail.com>
2025-05-20 14:07:19 -0300

    Correção função no ranking

--- cli-lib/src/ranking.c
+++ cli-lib/src/ranking.c
@@ -22,30 +22,6 @@ int compareScores(const void *a, const void *b) {
     return playerB->score - playerA->score;
 }
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "../include/ranking.h"
-
-#define MAX_PLAYERS 100
-#define MAX_LINE 128
-
-void salvarPontuacao(PlayerScore player) {
-    FILE *file = fopen("ranking.txt", "a");
-    if (file != NULL) {
-        fprintf(file, "%s %d\n", player.username, player.score);
-        fclose(file);
-    } else {
-        fprintf(stderr, "Erro ao abrir o arquivo de ranking.\n");
-    }
-}
-
-int compareScores(const void *a, const void *b) {
-    PlayerScore *playerA = (PlayerScore *)a;
-    PlayerScore *playerB = (PlayerScore *)b;
-    return playerB->score - playerA->score;
-}
-
 void exibirRanking() {
     PlayerScore *players = malloc(sizeof(PlayerScore) * MAX_PLAYERS);
     int count = 0;
